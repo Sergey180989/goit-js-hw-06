a1c3aa2db1461b035831c9c88038e9e7c5d1ebbb
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2023-09-05 22:31:41 +0300

    create_task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("ul");
+
+const arrayList = [];
+
+for (let i = 0; i < ingredients.length; i = i + 1) {
+  const newElement = document.createElement("li");
+  newElement.classList.add("item");
+  newElement.textContent = ingredients[i];
+  arrayList.push(newElement);
+}
+
+list.append(...arrayList);
