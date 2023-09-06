fda5a13515293e0f424ab986f0dddfd7044d0621
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2023-09-06 12:47:58 +0300

    create-task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector("button.change-color");
+console.log(changeColorBtn);
+
+const textOutput = document.querySelector("span.color");
+
+const bodyStyle = document.querySelector("body");
+console.log(bodyStyle);
+
+changeColorBtn.addEventListener("click", () => {
+  const backgroundColor = getRandomHexColor();
+  bodyStyle.style.backgroundColor = backgroundColor;
+  textOutput.textContent = backgroundColor;
+});
