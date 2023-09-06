ce352ac4e4f0a4e68e046a876d88a9e834635f75
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2023-09-06 11:34:02 +0300

    create-task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const resultObj = { email, password };
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please, fill in all the fields!");
+  }
+
+  console.log(resultObj);
+  event.currentTarget.reset();
+}
